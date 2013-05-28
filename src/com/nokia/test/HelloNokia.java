8aeaa34b333bf9c1adba845e07dd70d7686ba12a
Huayi Li <huayi.li@nokia.com>
2013-05-28 10:10:15 -0500

    adding a second note

--- src/com/nokia/test/HelloNokia.java
+++ src/com/nokia/test/HelloNokia.java
@@ -3,5 +3,6 @@ package com.nokia.test;
 public class HelloNokia {
 	public static void main(String[] args) {
 		System.out.println("Just a simple test: HelloNokia");
+		System.out.println("Just a second note: HelloHere");
 	}
 }

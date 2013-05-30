4f986acf70b09d022ca3a96ee2589516fbe1d452
howie <huayi.li@nokia.com>
2013-05-30 10:51:45 -0500

    changes in local master in eclipse

--- src/com/nokia/test/HelloNokia.java
+++ src/com/nokia/test/HelloNokia.java
@@ -4,5 +4,6 @@ public class HelloNokia {
 	public static void main(String[] args) {
 		System.out.println("Just a simple test: HelloNokia");
 		System.out.println("Just a second note: HelloHere");
+		System.out.println("cloned again from GitHub then add a third note : HelloGitHub from Eclipse Git tools");
 	}
 }
